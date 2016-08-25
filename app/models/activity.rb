3c5f81aaa004bd02369f660609555f796f1a4923
Jack Shales <jackshalesmusic@gmail.com>
2016-08-24 21:30:36 -0500

    add extra info to angular activity, start model methods

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -7,4 +7,11 @@ class Activity < ApplicationRecord
   def friendly_start_time
     start_time.strftime("%B %e %Y, %l:%M %p")
   end
+
+  def distance_in_miles
+    distance * 0.000621371
+  end
+
+  def friendly_duration
+  end
 end
