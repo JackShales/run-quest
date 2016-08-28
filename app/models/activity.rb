42c2cd04af2ef7b0fc31ef00b89e82ec8c65f0ce
Jack Shales <jackshalesmusic@gmail.com>
2016-08-28 13:47:52 -0500

    add activity model methods

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -13,5 +13,17 @@ class Activity < ApplicationRecord
   end
 
   def friendly_duration
+    hours = (duration / 3600).to_i
+    minutes = ((duration % 3600) / 60).to_i
+    seconds = (duration - ((hours * 3600) + (minutes * 60))).to_i
+    format("%02d:%02d:%02d", hours, minutes, seconds)
+  end
+
+  def avg_pace
+    raw_minutes = duration / 60
+    raw_pace = raw_minutes / distance_in_miles
+    pace_minutes = raw_pace.to_i
+    pace_seconds = (raw_pace - pace_minutes) * 60
+    format("%02d:%02d", pace_minutes, pace_seconds)
   end
 end
