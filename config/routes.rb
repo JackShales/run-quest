7d9b50797bdc05493b20ded437a4e99e86431219
Jack Shales <jackshalesmusic@gmail.com>
2016-09-06 12:09:54 -0500

    fix asset pipeline user show

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   # user dashboard
   get '/' => 'users#home'
   get '/users' => 'users#index'
-  get 'users/:id' => 'users#show'
+  get '/users/:id/' => 'users#show'
   get '/authorize' => 'users#authorize'
   # quest routes
   get '/quests' => 'quests#index'
   # get '/quests/new' => 'quests#new'
   post '/quests' => 'quests#create'
-  get '/quests/:id' => 'quests#show'
+  get '/quests/:id/' => 'quests#show'
   get '/quests/:id/edit' => 'quests#edit'
   patch '/quests/:id' => 'quests#update'
   delete '/quests/:id' => 'quests#destroy'
@@ -17,14 +17,14 @@ Rails.application.routes.draw do
   get '/activities' => 'activities#index'
   get '/activities/new' => 'activities#new'
   post '/activities' => 'activities#create'
-  get '/activities/:id' => 'activities#show'
+  get '/activities/:id/' => 'activities#show'
   get '/activities/:id/edit' => 'activities#edit'
-  patch '/activities/:id' => 'activities#update'
-  delete '/activities/:id' => 'activities#destroy'
+  patch '/activities/:id/' => 'activities#update'
+  delete '/activities/:id/' => 'activities#destroy'
   # friend routes
   get '/friends' => 'friends#index'
   post '/friends' => 'friends#create'
-  patch '/friends/:id' => 'friends#update'
+  patch '/friends/:id/' => 'friends#update'
 
   # Authentication routes
   get '/signup' => 'users#new'
@@ -38,10 +38,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/activities' => 'activities#index'
-      get '/activities/:id' => 'activities#show'
+      get '/activities/:id/' => 'activities#show'
 
       get '/quests' => 'quests#index'
-      get '/quests/:id' => 'quests#show'
+      get '/quests/:id/' => 'quests#show'
     end
   end
 end
